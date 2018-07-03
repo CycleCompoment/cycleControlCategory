07ee3e49d6266c9391c99cd5c72fac6cf7b97d72
ddy <15696564260@163.com>
2018-07-03 11:17:17 +0800

    Initial commit

---
+++ Example/cycleControlCategory/CycleViewController.h
@@ -0,0 +1,13 @@
+//
+//  CycleViewController.h
+//  cycleControlCategory
+//
+//  Created by cqcycle on 07/03/2018.
+//  Copyright (c) 2018 cqcycle. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CycleViewController : UIViewController
+
+@end
---
+++ Example/cycleControlCategory/CycleViewController.m
@@ -0,0 +1,29 @@
+//
+//  CycleViewController.m
+//  cycleControlCategory
+//
+//  Created by cqcycle on 07/03/2018.
+//  Copyright (c) 2018 cqcycle. All rights reserved.
+//
+
+#import "CycleViewController.h"
+
+@interface CycleViewController ()
+
+@end
+
+@implementation CycleViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
