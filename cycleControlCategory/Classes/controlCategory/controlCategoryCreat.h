8f438e6a54f44a54b3b4c4861fc1c56127b50483
ddy <15696564260@163.com>
2018-07-13 17:48:13 +0800

    添加创建分类的头文件

---
+++ cycleControlCategory/Classes/controlCategory/controlCategoryCreat.h
@@ -0,0 +1,24 @@
+//
+//  controlCategoryCreat.h
+//  Pods
+//
+//  Created by ddy on 2018/7/13.
+//
+
+#ifndef controlCategoryCreat_h
+#define controlCategoryCreat_h
+
+#ifdef __OBJC__
+
+#import "UIButton+CycleCreat.h"
+#import "UICollectionView+CycleCreat.h"
+#import "UILabel+CycleCreat.h"
+#import "UITableView+CycleCreat.h"
+#import "UITextField+CycleCreat.h"
+#import "UITextView+CycleCreat.h"
+
+#endif
+
+
+
+#endif /* controlCategoryCreat_h */
